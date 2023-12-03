cb6972927a8d2fdf2172ae63670fddcb91c61fc1
Everett Bryant <blackthought286@gmail.com>
2023-12-02 23:52:03 -0500

    answer for advent of code day one part one. Oiginally done in Kotlin

--- AdventOfCode2023CSharp/Program.cs
+++ AdventOfCode2023CSharp/Program.cs
@@ -3,10 +3,15 @@
 static Dictionary<int, int> FindCalibrationValuesInString(string str)
 {
         
+    // var alphaNumericToMatch = new Dictionary<string, int>()
+    // {
+    //     {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"1", 1}, {"2", 2},
+    //     {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}
+    // };
+    
     var alphaNumericToMatch = new Dictionary<string, int>()
     {
-        {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"1", 1}, {"2", 2},
-        {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}
+        {"1", 1}, {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}
     };
         
     var matchedWords = new Dictionary<int, int>();
